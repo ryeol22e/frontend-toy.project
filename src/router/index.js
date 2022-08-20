76e741847fc51e2502cffb677c15411808aa70d6
ryeol22e <ryeol22e@gmail.com>
2022-08-20 16:31:31 +0900

    소스정리

--- src/router/index.js
+++ src/router/index.js
@@ -8,14 +8,6 @@ import main from './main/main';
 import board from './board/board';
 
 const routes = [
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
 	{
 		path : '/:pathMatch(.*)*',
 		redirect : to=> {
@@ -51,6 +43,10 @@ router.beforeEach((to, from, next)=> {
 	}
 	
 	if(!authFlag) {
+		if(to.path==='/login' && token!=='') {
+			router.push({name : 'Main'});
+		}
+
 		next();
 	} else {
 		axios
